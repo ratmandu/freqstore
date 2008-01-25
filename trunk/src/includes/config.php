412c308e406c18474b789a2fbd98a16b5c592ca2
ratmandu <ratmandu@f0b39bf1-3143-0410-b69f-a59809720738>
2008-01-25 06:10:12 +0000

    Initial Commit. Files need work before pretty much any use

---
+++ trunk/src/includes/config.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * FreqStore - config.php
+ * Copyright (C) 2008 Justin Richards
+ * Author - Justin Richards <ratmandu@gmail.com>
+ * Released under the GNU General Public License v3
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+// Site Info
+define("SITE_NAME", "FreqStore");
+ 
+// database info
+define("DB_HOST", "hostname");
+define("DB_USER", "username");
+define("DB_PASS", "password");
+define("DB_NAME", "database");
+
+
+?>
